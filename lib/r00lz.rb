2c9cd8a658c5368eb168650f3a5b51fc24a0bea5
ahenning <ahenning@leapfrogonline.com>
2019-04-30 15:20:51 -0500

    Updated App class, created Controller class, updated Object

--- lib/r00lz.rb
+++ lib/r00lz.rb
@@ -2,13 +2,41 @@ require "r00lz/version"
 
 module R00lz
   class Error < StandardError; end
-  # Your code goes here...
 
   class App
-    def call(env)
-      [200,
-        {'Content-Type' => 'text/html'},
-        ["Hello from R00lz!"]]
+    def cont_and_act(env)
+      _, con, act, after = env["PATH_INFO"].split('/')
+      con = con.capitalize + "Controller"
+      [Object.const_get(con), act]
     end
+
+    def call(env)
+      kl, act = cont_and_act(env)
+      text = kl.new(env).send(act)
+      [200, {'Content-Type' => 'text/html'}, [text]]
+    end
+  end
+
+  class Controller
+    attr_reader :env
+
+    def initialize(env)
+      @env = env
+    end
+  end
+
+  def self.to_underscore(s)
+    s.gsub(
+      /([A-Z]+)([A-Z][a-z])/,
+      '\1_\2').gsub(
+      /([a-z\d])([A-Z])/,
+      '\1_\2').downcase
+  end
+end
+
+class Object
+  def self.const_missing(c)
+    require R00lz.to_underscore(c.to_s)
+    Object.const_get(c)
   end
 end
