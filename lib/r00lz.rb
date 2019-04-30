26b1367570eaaaf66e34b59e5527c9c5f89ec971
ahenning <ahenning@leapfrogonline.com>
2019-04-30 14:39:33 -0500

    Updated gemspec and r00lz.rb

--- lib/r00lz.rb
+++ lib/r00lz.rb
@@ -3,4 +3,12 @@ require "r00lz/version"
 module R00lz
   class Error < StandardError; end
   # Your code goes here...
+
+  class App
+    def call(env)
+      [200,
+        {'Content-Type' => 'text/html'},
+        ["Hello from R00lz!"]]
+    end
+  end
 end
